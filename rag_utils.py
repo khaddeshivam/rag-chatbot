a4e28b0bd1db65c3c12ea7be15f5f729f8468464
Shivam Khadde <132277568+khaddeshivam@users.noreply.github.com>
2026-04-29 14:28:40 +0530

    Update rag_utils.py

--- rag_utils.py
+++ rag_utils.py
@@ -12,12 +12,12 @@ from langchain_community.document_loaders import (
 )
 
 from langchain_community.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, AzureOpenAIEmbeddings
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 
 load_dotenv()
 
